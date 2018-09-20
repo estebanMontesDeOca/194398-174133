b22b69884d8f1b7784470b06cadecbd4e5d16c6c
Esteban Montes de Oca <steineres1995@live.com>
2018-09-20 15:26:24 -0300

    Team Entity was made

---
+++ MatchesOfSports.Domain/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MatchesOfSports.Domain
+{
+    public class Team
+    {
+        private string name{get; set;}
+        private string photoUrl{get;set;}
+
+        
+        public Team(){
+            name="noName";
+            photoUrl="noPhoto";
+        }
+        public Team(string aName, string aUrl)
+        {
+            name = aName;
+            photoUrl = aUrl;
+        }
+    }
+}
