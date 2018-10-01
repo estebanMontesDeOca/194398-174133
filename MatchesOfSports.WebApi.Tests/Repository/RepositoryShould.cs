7daf2d19ff11bc17c2da3494bf1f559bd9fa22c4
Esteban Montes de Oca <steineres1995@live.com>
2018-10-01 13:16:10 -0300

    FailToCreatIfContextIsNull

--- MatchesOfSports.WebApi.Tests/Repository/RepositoryShould.cs
+++ MatchesOfSports.WebApi.Tests/Repository/RepositoryShould.cs
@@ -13,6 +13,7 @@ namespace MatchesOfSports.WebApi.Tests
             MatchesOfSportsContext context = null;
 
             MatchesOfSports<Sport> repository = new RepositoryOf<Sport>(context);
+        
         }
     }
 }
