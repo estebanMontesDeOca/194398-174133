828b0a7a0b782bf32524081a8093b3938efb84b7
Esteban Montes de Oca <steineres1995@live.com>
2018-10-09 01:47:23 -0300

    Finish on services

--- MatchesOfSports.BusinessLogic/Services/MatchService.cs
+++ MatchesOfSports.BusinessLogic/Services/MatchService.cs
@@ -30,9 +30,61 @@ namespace MatchesOfSports.BusinessLogic.Services
                 throw new InvalidOperationException("Could not get all matches - Data Base empty");
             } 
         }
-        Match GetMatchById(Guid id);
-        bool DeleteMatch(Guid Id);
-        bool CreateMathc(Match newMatch);
-        bool UpdateMatch(string userName, User updatedUser);
+        public Match GetMatchById(Guid id)
+        {
+            try{
+                return unitOfWork.MatchRepository.Get(id);
+            }catch(ArgumentNullException){
+                throw new InvalidOperationException("Could not get match - Match does not exist");
+            }
+        }
+        public bool DeleteMatch(Guid Id)
+        {
+            try{    
+                Team toDelete= GetMatchById(id);
+                toDelete.WasDeleted = true;
+                UpdateMatch(id,toDelete);
+                return true;
+            }catch(ArgumentNullException)
+            {
+                throw new InvalidOperationException("Could not delet the match - Match does not exist"); 
+            }
+        }
+
+        public bool ExistMatch(Guid id)
+        {
+            return unitOfWork.MatchRepository.Get(id) != null;
+        }
+        public bool CreateMatch(Match newMatch)
+        {
+            if(ExistMatch(newMatch.MatchId)){
+                throw new InvalidOperationException("Could not create new match - Match  already exists");
+            }else{
+                unitOfWork.MatchRepository.Add(newMatch);
+                unitOfWork.Save();
+                return true;
+            }
+
+        }
+        public bool UpdateMatch(Guid id, Match updatedMatch)
+        {
+            try{
+                Match oldMatch =GetMatchtById(id);
+                oldMatch.MatchtId = updatedMatch.MatchtId;
+                oldMatch.DateAndTime = updatedMatch.DateAndTime;
+                oldMatch.TemaOne= updatedMatch.TeamOne;
+                oldMatch.TeamTwo = updatedMatch.TeamTwo;
+                oldMatch.Comments = updatedMatch.Comments;
+                oldMatch.KindOfSport = updatedMatch.KindOfSport;
+                oldMatch.WasDeleted = updatedMatch.WasDeleted;
+
+                unitOfWork.MatchRepository.Update(oldMatch);
+                unitOfWork.Save();
+                return true;
+            }catch(ArgumentNullException)
+            {
+                throw new InvalidOperationException("Could not update match - Match  does not exists");
+            }
+        }
 
     }
