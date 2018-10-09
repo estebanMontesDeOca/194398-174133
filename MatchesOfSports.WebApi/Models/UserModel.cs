7e878574ab9e5621003c69e4a90fb28eb7566d7f
Esteban Montes de Oca <steineres1995@live.com>
2018-10-09 16:20:16 -0300

    UserModel was implemented

---
+++ MatchesOfSports.WebApi/Models/UserModel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MatchesOfSports.Domain;
+
+namespace MatchesOfSports.WebApi.Models
+{
+    public class CommentModel : Model<Comment, CommentModel>
+    {
+        public Guid CommentId { get; set; }
+        public string TheComment { get; set; }
+        public User UserWhoComment { get; set; }
+
+        public CommentModel(Comment entity)
+        {
+            SetModel(entity);
+        }
+
+        public override Comment ToEntity() => new Comment()
+        {
+            CommentId = this.CommentId,
+            TheComment = this.TheComment,
+            UserWhoComment = this.UserWhoComment,
+        };
+
+        protected override CommentModel SetModel(Comment entity)
+        {
+            CommentId = entity.CommentId;
+            TheComment = entity.TheComment;
+            UserWhoComment = entity.UserWhoComment;
+            return this;
+        }
+
+    }
+}
